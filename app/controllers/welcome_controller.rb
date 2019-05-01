1ee4e8c073d0fb04e61142577e8aa63293a89c1e
Mark Modak <mmodak@mmodak.local>
2019-05-01 17:47:12 -0600

    Can you find my API keys?

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
 
   def home
+    gon.mountain = Rails.application.credentials.mountain_secret
+    gon.weather = Rails.application.credentials.weather_secret
+    gon.map = Rails.application.credentials.map_secret
   end
 
-  def map
-  end 
+  def test
+    gon.cred = Rails.application.credentials.mountain_secret
+  end
 
 end
