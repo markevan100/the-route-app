f5a22e9792372b87a951a55451656cf6b6855d36
Mark Modak <mmodak@mmodak.local>
2019-05-12 11:33:11 -0600

    devise doesn't have a db

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   get 'welcome/yourroute', to: 'welcome#yourroute'
   post 'welcome/index', to: 'welcome#post'
+
+  get 'welcome/comments', to: 'welcome#comments'
 
 
 end
